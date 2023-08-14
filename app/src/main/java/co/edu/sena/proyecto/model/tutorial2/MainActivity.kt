97cdfb6e441bb7307ce4b8efedffb29cbae2bc7d
ycystorm <jeancarloquirogahernandez958@gmail.com>
2023-08-13 22:55:14 -0500

    lista de contactos terminada

---
+++ app/src/main/java/co/edu/sena/proyecto/model/tutorial2/MainActivity.kt
@@ -0,0 +1,141 @@
+package co.edu.sena.proyecto.model.tutorial2
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.edu.sena.proyecto.model.tutorial2.ui.theme.Tutorial2Theme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Tutorial2Theme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column {
+                        MessageCard(Message("Lina", "Te envio un mensaje"))
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.bokuno),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            label = ""
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.labelMedium
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+
+}
+
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    Tutorial2Theme {
+        Surface {
+            MessageCard(
+                msg = Message(" Lina", "Take a look at Jetpack Compose, it's great!")
+            )
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    Tutorial2Theme {
+        Conversation(Tutorial.conversationSample)
+    }
+}
---
+++ app/src/main/java/co/edu/sena/proyecto/model/tutorial2/Tutorial.kt
@@ -0,0 +1,72 @@
+package co.edu.sena.proyecto.model.tutorial2
+
+object Tutorial {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "samuel",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "samuel",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "samuel",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "samuel",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "samuel",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "samuel",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "samuel",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "samuel",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "samuel",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "samuel",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "samuel",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "samuel",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "samuel",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
+}
